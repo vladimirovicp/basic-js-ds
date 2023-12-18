b23f85619add4bb24727cde8fe2f53780b43cabd
vladimirovicp <vladimirovicp70@gmail.com>
2023-12-18 21:01:04 +0400

    add: list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  //удаление элементов
+  removeAt(position) {
+    if (position < 0 || position >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+
+    if (position === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = current.next;
+    }
+
+    this.length--;
+    return current.value;
+  }
+
+  // удалить элемент
+  remove(element) {
+    this.removeAt( this.indexOf(element) );
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  print() {
+    let current = this.head;
+
+    while(current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+}
+
+function removeKFromList(l, k) {
+  let list = new LinkedList();
+  list.head = l;
+
+  while( list.indexOf(k) !== -1){
+    list.remove(k)
+  }
+  return list;
+
 }
 
 module.exports = {
--- src/remove-from-list_main.js
+++ src/remove-from-list_main.js
@@ -1,31 +1,3 @@
-//const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Given a singly linked list of integers l and an integer k,
- * remove all elements from list l that have a value equal to k.
- *
- * Учитывая односвязный список целых чисел l и целого числа k,
- * удалить из списка l все элементы, имеющие значение, равное k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-
 class ListNode {
   constructor(x) {
     this.value = x;
@@ -33,9 +5,124 @@ class ListNode {
   }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      let current = this.head;
+
+      // move to the last node
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = new ListNode(value);
+    }
+
+    this.length++;
+  }
+
+  //удаление элементов
+  removeAt(position) {
+    if (position < 0 || position >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+
+    if (position === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = current.next;
+    }
+
+    this.length--;
+    return current.value;
+  }
+
+  // удалить элемент
+  remove(element) {
+    this.removeAt( this.indexOf(element) );
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  print() {
+    let current = this.head;
+
+    while(current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+}
+
 
 function removeKFromList(l, k ) {
-  console.log(l)
+
+  let list = new LinkedList();
+
+  l.forEach(item=>{
+    list.add(item)
+  })
+
+
+  list.print();
+
+  while( list.indexOf(k) !== -1){
+    list.remove(k)
+  }
+
+
+  //console.log('indexOf',list.indexOf(k));
+  // console.log('\n')
+  //
+  // list.remove(k)
+  // list.print();
+  //
+  console.log('\n')
+  // list.remove(k)
+  list.print();
+  //
+  // console.log('\n')
+  // list.remove(k)
+  // list.print();
+  //
+  // console.log('indexOf',list.indexOf(k));
+
+  //list.print();
+  //console.log(l)
+
+
 }
 
 const l = [3, 1, 2, 3, 4, 5];
